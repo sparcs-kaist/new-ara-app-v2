1a3a42aebff2c4bd4616c426ecec7c27664a845a
sum37 <82599165+sum37@users.noreply.github.com>
2024-10-07 21:22:30 +0900

    bottom-navigation

--- component/ShortCut.tsx
+++ component/ShortCut.tsx
@@ -5,7 +5,7 @@ const ShortCut = ({ fontSize = 16, text = 'Button', color = '#000', onPress }) =
   return (
     <TouchableOpacity
         onPress={onPress}
-        style ={{flexShrink:1, flexDirection: 'row', backgroundColor: 'red', margin: 10, alignItems: 'center'}}>
+        style ={{flexShrink:1, flexDirection: 'row', margin: 10, alignItems: 'center'}}>
       <Text style={{fontSize: fontSize, color: color}}>{text}</Text>
       <Image
         source = {require('../assets/right_chevron1.png')}
